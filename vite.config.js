7e8cbc05a1ac4c759e60c27d3467b470dfe8151f
ecthelionofthefountain-510 <lundstedt.kevin@gmail.com>
2025-06-02 11:47:13 +0200

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my_portfolio/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 })
+
+
+
+// när man ska nygga för github pages kör:
+
+// VITE_BASE_PATH=/my_portfolio/ npm run build
